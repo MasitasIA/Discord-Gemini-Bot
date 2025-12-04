f566c9c1b75811b96482beee55a473a5f62a1b1c
Masitas <nereoschmidt@gmail.com>
2025-12-03 23:13:36 -0300

    Arregle el modelo de la Gemini

--- gemini.py
+++ gemini.py
@@ -1,12 +1,15 @@
-import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY")
 
+client = genai.Client(api_key=api_key)
+
 def gemini_query(query: str):
-    genai.configure(api_key=api_key)
-    model = genai.GenerativeModel("gemini-lash-latest")
-    response = model.generate_text(f"Responde en menos de 1800 caracteres: {query}")
+    response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=f"Trata de responder en menos de 1800 caracteres a este texto: {query}",
+    )
     return response.text
